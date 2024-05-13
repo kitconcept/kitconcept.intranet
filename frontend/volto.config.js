2ba15ca7335d9e68ea7f21b51f89b2c68de749e9
Timo Stollenwerk <stollenwerk@kitconcept.com>
2024-05-13 12:19:36 +0200

    Add blocks

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,5 +1,16 @@
-const addons = ['@kitconcept/volto-intranet'];
-const theme = '';
+const addons = [
+  '@eeacms/volto-accordion-block',
+  '@kitconcept/volto-button-block',
+  '@kitconcept/volto-heading-block',
+  '@kitconcept/volto-highlight-block',
+  '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-separator-block',
+  '@kitconcept/volto-slider-block',
+  '@kitconcept/volto-light-theme',
+  '@kitconcept/volto-intranet',
+];
+
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,

03ae403afef41662036f3d1066139e2068272baf
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-02-27 17:01:37 +0100

    Push missing file

---
+++ frontend/packages/volto-intranet/src/index.ts
@@ -0,0 +1,10 @@
+import { ConfigType } from '@plone/registry';
+import installSettings from './config/settings';
+
+const applyConfig = (config: ConfigType) => {
+  installSettings(config);
+
+  return config;
+};
+
+export default applyConfig;

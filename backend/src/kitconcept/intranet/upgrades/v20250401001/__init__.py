482a851b1dacafa2e9d65704ef26fe75dd964bff
Érico Andrei <ericof@gmail.com>
2025-04-02 09:13:27 -0300

    Refactor cmfplone upgrade function

--- backend/src/kitconcept/intranet/upgrades/v20250401001/__init__.py
+++ backend/src/kitconcept/intranet/upgrades/v20250401001/__init__.py
@@ -1,12 +1,7 @@
-from kitconcept.intranet import logger
-from plone import api
-from Products.CMFPlone.MigrationTool import MigrationTool
+from kitconcept.intranet.upgrades.helpers import upgrade_cmfplone
 from Products.GenericSetup.tool import SetupTool
 
 
 def upgrade_plone_611(setup_tool: SetupTool):
     """Upgrade Products.CMFPlone to version 6.1.1."""
-    request = api.env.getRequest()
-    migration_tool: MigrationTool = api.portal.get_tool("portal_migration")
-    result = migration_tool.upgrade(request, dry_run=False, swallow_errors=True)
-    logger.info(f"Upgrade to Plone 6.1.1. {result}")
+    upgrade_cmfplone()

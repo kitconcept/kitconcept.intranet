1f235669e74530df36c13d796baccf55a668cab2
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-06-06 17:17:23 +0200

    Fix last test

--- backend/tests/registration/test_distribution.py
+++ backend/tests/registration/test_distribution.py
@@ -14,7 +14,6 @@ class TestDistribution:
                 "description",
                 "Site created with A Plone distribution for Intranets with Plone. Created by kitconcept.",  # noQA: E501
             ),
-            ("exclude_from_nav", False),
         ],
     )
     def test_plone_site_attributes(self, portal, attr, expected):

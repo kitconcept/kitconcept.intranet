093be2df54a7cb60ac3b793c64e5864f923cd4d4
iRohitSingh <61353484+iRohitSingh@users.noreply.github.com>
2025-06-10 19:58:58 +0530

    Add example content of Sticky menu ,Person content type and update footer example content and background color (#105)
    * Add example content of Sticky menu ,Person content type and update footer example content and background color
    
    * Add change log
    
    * Update example content
    
    * Add a transaction.savepoint after the content importer run to avoid POSKey errors
    
    ---------
    
    Co-authored-by: Víctor Fernández de Alba <sneridagh@gmail.com>
    Co-authored-by: Érico Andrei <ericof@gmail.com>

--- backend/src/kitconcept/intranet/handlers.py
+++ backend/src/kitconcept/intranet/handlers.py
@@ -2,11 +2,49 @@ from copy import deepcopy
 from kitconcept.intranet import logger
 from kitconcept.intranet.utils.authentication import setup_authentication
 from plone import api
+from plone.dexterity.schema import SCHEMA_CACHE
 from plone.distribution.core import Distribution
-from plone.distribution.handler import default_handler
 from plone.distribution.utils.data import convert_data_uri_to_b64
+from plone.exportimport.importers import get_importer
 from Products.CMFPlone.Portal import PloneSite
 from Products.CMFPlone.WorkflowTool import WorkflowTool
+
+import transaction
+
+
+def default_handler(
+    distribution: Distribution, site: PloneSite, answers: dict
+) -> PloneSite:
+    """Default handler to create a new site."""
+    # Process answers
+    profiles = distribution.profiles
+    setup_content = answers.get("setup_content", False)
+    setup_tool = site["portal_setup"]
+    for profile_id in profiles:
+        setup_tool.runAllImportStepsFromProfile(f"profile-{profile_id}")
+
+    # Add default content if needed
+    if setup_content:
+        # If there is no savepoint most tests fail with a PosKeyError
+        transaction.savepoint(optimistic=True)
+        contents = distribution.contents
+        # First process any content profiles
+        content_profiles = contents["profiles"]
+        for profile_id in content_profiles:
+            setup_tool.runAllImportStepsFromProfile(f"profile-{profile_id}")
+        # Process content import from json
+        content_json_path = contents["json"]
+        if content_json_path:
+            # Invalidate the schema cache to make sure we get up to date behaviors.
+            # Normally this happens on commit, but we didn't commit yet.
+            SCHEMA_CACHE.clear()
+            importer = get_importer(site)
+            importer.import_site(content_json_path)
+            # Create a savepoint to ensure the import is atomic
+            transaction.savepoint(optimistic=True)
+            # Commit the transaction to finalize the import
+            transaction.commit()
+    return site
 
 
 def pre_handler(answers: dict) -> dict:

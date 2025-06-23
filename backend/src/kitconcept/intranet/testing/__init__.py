059fa40ab8095991a3fc833718055fa8ffc46521
Érico Andrei <ericof@gmail.com>
2025-06-23 19:42:58 -0300

    Implement robotframework support for acceptance tests

---
+++ backend/src/kitconcept/intranet/testing/__init__.py
@@ -0,0 +1,10 @@
+from kitconcept.intranet.testing.base import FUNCTIONAL_TESTING
+from kitconcept.intranet.testing.base import INTEGRATION_TESTING
+from kitconcept.intranet.testing.robot import ROBOT_TESTING
+
+
+__all__ = [
+    "FUNCTIONAL_TESTING",
+    "INTEGRATION_TESTING",
+    "ROBOT_TESTING",
+]

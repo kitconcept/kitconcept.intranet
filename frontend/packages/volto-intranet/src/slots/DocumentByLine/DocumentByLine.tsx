1c21e8b4e84cc2ce9e4df811352c14ca235b6397
Tishasoumya-02 <tishasoumya@gmail.com>
2025-06-24 18:37:36 +0530

    fix: naming conventions

--- frontend/packages/volto-intranet/src/slots/DocumentByLine/DocumentByLine.tsx
+++ frontend/packages/volto-intranet/src/slots/DocumentByLine/DocumentByLine.tsx
@@ -4,9 +4,9 @@ import { expandToBackendURL } from '@plone/volto/helpers/Url/Url';
 import { useEffect, useState } from 'react';
 
 const DocumentByLine = ({ content }: { content: Content }) => {
-  const [creatorsProfile, setCreatorInfo] = useState<string[][]>([]);
+  const [creatorProfiles, setCreatorProfiles] = useState<string[][]>([]);
 
-  const creatorProfile = async (username: string): Promise<string> => {
+  const getCreatorHomePage = async (username: string): Promise<string> => {
     try {
       const response = await fetch(expandToBackendURL(`/@users/${username}`));
       const data: User = await response.json();
@@ -17,19 +17,19 @@ const DocumentByLine = ({ content }: { content: Content }) => {
   };
 
   useEffect(() => {
-    const fetchCreatorInfo = async () => {
+    const fetchCreatorProfiles = async () => {
       const result = await Promise.all(
         content.creators.map(async (user) => {
-          const abt = await creatorProfile(user);
+          const abt = await getCreatorHomePage(user);
           return [user, abt || ''];
         }),
       );
-      setCreatorInfo(result);
+      setCreatorProfiles(result);
     };
-    fetchCreatorInfo();
+    fetchCreatorProfiles();
   }, [content.creators]);
 
-  const dateStringConvert = (ISOstring: string): String => {
+  const formatDate = (ISOstring: string): String => {
     const date: Date = new Date(ISOstring);
     return date.toLocaleDateString('en-US', {
       year: 'numeric',
@@ -38,38 +38,38 @@ const DocumentByLine = ({ content }: { content: Content }) => {
     });
   };
 
-  const formatted_effective_date = dateStringConvert(content?.effective ?? '');
-  const formatted_modified_date = dateStringConvert(content.modified);
+  const formattedEffectiveDate = formatDate(content?.effective ?? '');
+  const formattedModifiedDate = formatDate(content.modified);
 
   return (
     <div className="documentByLine">
       <span>
         by{' '}
-        {creatorsProfile.map(([name, url], index) =>
+        {creatorProfiles.map(([name, url], index) =>
           url ? (
             <React.Fragment key={index}>
               <a className="byAuthor" href={url}>
                 {name}
               </a>
-              {index < creatorsProfile.length - 1 && ' '}
+              {index < creatorProfiles.length - 1 && ' '}
             </React.Fragment>
           ) : (
             <React.Fragment key={index}>
               <span className="byAuthor">{name}</span>
-              {index < creatorsProfile.length - 1 && ' '}
+              {index < creatorProfiles.length - 1 && ' '}
             </React.Fragment>
           ),
         )}
       </span>
       <span className="documentPublished">
         {content.review_state === 'published' ? (
-          <span> — published {formatted_effective_date} </span>
+          <span> — published {formattedEffectiveDate} </span>
         ) : (
           ''
         )}
       </span>
       <span className="documentModified">
-        , last modified {formatted_modified_date}
+        , last modified {formattedModifiedDate}
       </span>
     </div>
   );

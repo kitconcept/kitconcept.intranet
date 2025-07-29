64184791c9964410edaff0673563c6e4590ddf76
Érico Andrei <ericof@gmail.com>
2025-07-29 14:53:43 -0300

    Add Location content type to the navigation

--- backend/tests/setup/test_setup_install.py
+++ backend/tests/setup/test_setup_install.py
@@ -1,4 +1,8 @@
 from kitconcept.intranet import PACKAGE_NAME
+from plone import api
+from typing import Any
+
+import pytest
 
 
 class TestSetupInstall:
@@ -17,3 +21,48 @@ class TestSetupInstall:
     def test_latest_version(self, profile_last_version):
         """Test latest version of default profile."""
         assert profile_last_version(self.profile_id) == "20250729001"
+
+
+class TestRegistrySettings:
+    @pytest.fixture(autouse=True)
+    def _setup(self, portal_class):
+        """Setup the portal for registry settings tests."""
+        self.portal = portal_class
+
+    @pytest.mark.parametrize(
+        "record,oper,value",
+        [
+            ("plone.displayed_types", "in", "Location"),
+            ("plone.app.querystring.field.location.title", "eq", "label_place"),
+            ("plone.app.querystring.field.location.description", "eq", "help_place"),
+            ("plone.app.querystring.field.location.enabled", "is", True),
+            ("plone.app.querystring.field.location.sortable", "is", False),
+            (
+                "plone.app.querystring.field.location.operations",
+                "in",
+                "plone.app.querystring.operation.selection.any",
+            ),
+            (
+                "plone.app.querystring.field.location.vocabulary",
+                "in",
+                "kitconcept.intranet.vocabularies.location",
+            ),
+            ("plone.app.querystring.field.location.group", "eq", "Taxonomy"),
+        ],
+    )
+    def test_registry_settings(self, record: str, oper: str, value: Any):
+        """Test registry settings."""
+        record_value = api.portal.get_registry_record(record, default=None)
+        match oper:
+            case "in":
+                assert value in record_value
+            case "not in":
+                assert value not in record_value
+            case "eq":
+                assert value == record_value
+            case "ne":
+                assert value != record_value
+            case "is":
+                assert record_value is value
+            case "is not":
+                assert record_value is not value

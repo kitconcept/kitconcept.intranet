99af7ace8decabdffb3058683f83f741618be7f2
iFlameing <ialokkumarsingh0@gmail.com>
2025-08-04 18:37:25 +0530

    refactor vocabularies into base class

--- backend/src/kitconcept/intranet/vocabularies/location.py
+++ backend/src/kitconcept/intranet/vocabularies/location.py
@@ -1,52 +1,20 @@
-from kitconcept.intranet.vocabularies.base import CatalogVocabulary
+from kitconcept.intranet.vocabularies.base import BaseRelationVocabulary
+from kitconcept.intranet.vocabularies.base import BaseSimpleVocabulary
 from plone import api
-from plone.dexterity.content import DexterityContent
 from zope.interface import implementer
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
 
 
 @implementer(IVocabularyFactory)
-class LocationsRelationVocabulary:
-    """Vocabulary of available Location objects."""
-
-    def query(self, context: DexterityContent) -> dict:
-        """Query for Presenters."""
-        return {
-            "context": context,
-            "portal_type": "Location",
-            "sort_on": "sortable_title",
-        }
-
-    @staticmethod
-    def prepare_title(result) -> str:
-        """Return a friendly value to be used in the vocabulary."""
-        return result.Title
-
-    def __call__(
-        self, context: DexterityContent, query: dict | None = None
-    ) -> CatalogVocabulary:
-        query = self.query(context)
-        results = api.content.find(**query)
-        terms = []
-        for result in results:
-            title = self.prepare_title(result)
-            terms.append(SimpleTerm(result.getObject(), result.UID, title))
-        return CatalogVocabulary(terms)
+class LocationsRelationVocabulary(BaseRelationVocabulary):
+    def __init__(self):
+        super().__init__("Location")
 
 
 LocationsRelationVocabularyFactory = LocationsRelationVocabulary()
 
 
 @provider(IVocabularyFactory)
-def locations_vocabulary(context: DexterityContent) -> SimpleVocabulary:
-    """Returns a vocabulary with all Location objects."""
-    brains = api.content.find(
-        context=context, portal_type="Location", sort_on="sortable_title"
-    )
-    terms: list[SimpleTerm] = []
-    for brain in brains:
-        terms.append(SimpleTerm(brain.UID, brain.UID, brain.Title))
-    return SimpleVocabulary(terms)
+def locations_vocabulary(context):
+    return BaseSimpleVocabulary("Location")(context)

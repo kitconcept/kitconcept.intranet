bba200a490b6cb76e50674373a63bc71ba9a559d
David Glick <david@glicksoftware.com>
2025-08-06 12:13:30 -0700

    lint

--- backend/src/kitconcept/intranet/services/byline.py
+++ backend/src/kitconcept/intranet/services/byline.py
@@ -2,14 +2,13 @@ from plone import api
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
 from zope.component import adapter
-from zope.interface import implementer
 from zope.interface import Interface
+from zope.interface import implementer
 
 
 @implementer(IExpandableElement)
 @adapter(Interface, Interface)
 class BylineExpander:
-
     def __init__(self, context, request):
         self.context = context
         self.request = request

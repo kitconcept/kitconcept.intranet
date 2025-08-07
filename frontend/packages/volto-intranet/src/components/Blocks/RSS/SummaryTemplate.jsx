1e2838276138bf2ee962eb872f378f6db7675c6d
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2025-08-07 16:09:18 -0600

    add rss block templates

---
+++ frontend/packages/volto-intranet/src/components/Blocks/RSS/SummaryTemplate.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useIntl, defineMessages } from 'react-intl';
+import Card from '@kitconcept/volto-light-theme/primitives/Card/Card';
+import RssSummary from '../../Summary/RssSummary';
+
+const messages = defineMessages({
+  readMore: { id: 'rss_read_more', defaultMessage: 'Read more' },
+  noResults: {
+    id: 'rss_no_results',
+    defaultMessage: 'No results from RSS feed.',
+  },
+});
+
+const RssSummaryTemplate = ({ items, data, isEditMode }) => {
+  const intl = useIntl();
+  return (
+    <>
+      {data.title && <h2 className="headline">{data.title}</h2>}
+
+      {items?.length > 0 ? (
+        <div className="items">
+          {items.map((item, index) => (
+            <div
+              className="listing-item has--align--left rss-item"
+              key={item.url || index}
+            >
+              <Card href={!isEditMode && item.url ? item.url : null}>
+                {(item.enclosure?.url || item.image) && (
+                  <Card.Image
+                    src={item.enclosure?.url || item.image}
+                    alt={item.title || 'RSS item image'}
+                    showPlaceholderImage={true}
+                  />
+                )}
+                <Card.Summary>
+                  <RssSummary item={item} HeadingTag="h2" />
+                </Card.Summary>
+              </Card>
+            </div>
+          ))}
+        </div>
+      ) : data.feeds?.length > 0 && data.feeds.some((feed) => feed.url) ? (
+        <div className="no-rss-feed-results">
+          {intl.formatMessage(messages.noResults)}
+        </div>
+      ) : null}
+    </>
+  );
+};
+RssSummaryTemplate.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+  data: PropTypes.object.isRequired,
+  isEditMode: PropTypes.bool,
+};
+
+export default RssSummaryTemplate;
---
+++ frontend/packages/volto-intranet/src/config/blocks.ts
@@ -0,0 +1,21 @@
+import type { ConfigType } from '@plone/registry';
+import RssDefaultTemplate from '../components/Blocks/RSS/DefaultTemplate';
+import RssGridTemplate from '../components/Blocks/RSS/GridTemplate';
+import RssSummaryTemplate from '../components/Blocks/RSS/SummaryTemplate';
+export default function install(config: ConfigType) {
+  config.blocks.blocksConfig.rssBlock.templates = {
+      default: {
+        label: 'List',
+        template: RssDefaultTemplate,
+      },
+      grid: {
+        label: 'Grid',
+        template: RssGridTemplate,
+      },
+      summary: {
+        label: 'List with image',
+        template: RssSummaryTemplate,
+      },
+    };
+  return config;
+}

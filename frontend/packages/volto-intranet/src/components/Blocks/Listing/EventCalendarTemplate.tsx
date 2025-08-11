2c108bc0e9a32e1ba8f04ea6a4f1cfac439e80bc
Tishasoumya-02 <tishasoumya@gmail.com>
2025-08-11 18:31:44 +0530

    feature: listing-with-date

---
+++ frontend/packages/volto-intranet/src/components/Blocks/Listing/EventCalendarTemplate.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '@kitconcept/volto-light-theme/primitives/Card/Card';
+import DefaultSummary from '@kitconcept/volto-light-theme/components/Summary/DefaultSummary';
+import cx from 'classnames';
+
+type IntlType = {
+  intl: {
+    locale: string;
+  };
+};
+
+const EventItem = ({
+  item,
+  lang,
+  isEditMode,
+}: {
+  item: any;
+  lang: string;
+  isEditMode: boolean;
+}) => {
+  const today = new Date();
+
+  const formatter = new Intl.DateTimeFormat(lang, {
+    year: 'numeric',
+    month: 'short',
+  });
+  const headFormatter = new Intl.DateTimeFormat(lang, {
+    day: 'numeric',
+    year: 'numeric',
+    month: 'long',
+  });
+
+  const start =
+    item['@type'] === 'Event'
+      ? item.start
+        ? new Date(item.start)
+        : null
+      : item.Effective
+        ? new Date(item.Effective)
+        : new Date(item.CreationDate);
+  const end =
+    item['@type'] === 'Event' && (item.end ? new Date(item.end) : null);
+  const notSameDay = end && start?.getDate() !== end?.getDate();
+  const formattedStartDate = start ? formatter.format(start) : '';
+  const formattedEndDate = end ? formatter.format(end) : '';
+  const formattedHeaderDate = !end
+    ? (start ? headFormatter.format(start) : '')
+    : (start && end ? headFormatter.formatRange(start, end) : '');
+
+  return (
+    <div className="card-listing">
+      <Card href={isEditMode ? '' : item['@id']} className="event-card">
+        <Card.Image>
+          <div className={cx('date-inset', { 'has-end-date': notSameDay })}>
+            <div className="day">
+              {String(start?.getDate()).padStart(2, '0')}
+            </div>
+            <div className="month">{formattedStartDate}</div>
+            {item['@type'] === 'Event' && notSameDay && (
+              <>
+                <div className="separator"></div>
+                <div className="day">
+                  {String(end.getDate()).padStart(2, '0')}
+                </div>
+                <div className="month">{formattedEndDate}</div>
+              </>
+            )}
+          </div>
+        </Card.Image>
+        <Card.Summary>
+          <div className="headline calendar">
+            <span className="day">{formattedHeaderDate}</span>
+            {item.head_title && (
+              <span className="event-title">{item.head_title}</span>
+            )}
+          </div>
+          <DefaultSummary item={item} HeadingTag="h2" />
+        </Card.Summary>
+      </Card>
+    </div>
+  );
+};
+
+const EventCalenderTemplate = (props: any) => {
+  const lang = useSelector((state: IntlType) => state.intl.locale);
+  console.log(props, 'EventCalendarTemplate items');
+  return (
+    <div className="event-calendar items">
+      {props.items.map((item: any, index: number) => (
+        <EventItem
+          key={index}
+          item={item}
+          lang={lang}
+          isEditMode={props.isEditMode}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default EventCalenderTemplate;
---
+++ frontend/packages/volto-intranet/src/config/blocks.ts
@@ -0,0 +1,26 @@
+
+import { ConfigType } from '@plone/registry';
+import EventCalendarTemplate from '../components/Blocks/Listing/EventCalendarTemplate';
+
+export default function install(config: ConfigType) {
+    const listingBlockVariations = [
+        ...config.blocks.blocksConfig.listing.variations || [],
+        {
+            id: 'listDate',
+            title: 'list with Date',
+            template: EventCalendarTemplate,
+        },
+
+    ].filter((variation) => !!variation);
+
+    config.blocks.blocksConfig.listing = {
+        ...config.blocks.blocksConfig.listing,
+        variations: listingBlockVariations,
+    };
+
+    config.blocks.blocksConfig.listing = {
+        ...config.blocks.blocksConfig.listing,
+        variations: listingBlockVariations
+    };
+    return config;
+}

ffee2acfebd5b5560e8396a640fd201a9531f7dc
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2025-08-12 16:25:52 -0600

    lint

--- frontend/packages/volto-intranet/src/config/blocks.ts
+++ frontend/packages/volto-intranet/src/config/blocks.ts
@@ -4,18 +4,18 @@ import RssGridTemplate from '../components/Blocks/RSS/GridTemplate';
 import RssSummaryTemplate from '../components/Blocks/RSS/SummaryTemplate';
 export default function install(config: ConfigType) {
   config.blocks.blocksConfig.rssBlock.templates = {
-      default: {
-        label: 'List',
-        template: RssDefaultTemplate,
-      },
-      grid: {
-        label: 'Grid',
-        template: RssGridTemplate,
-      },
-      summary: {
-        label: 'List with image',
-        template: RssSummaryTemplate,
-      },
-    };
+    default: {
+      label: 'List',
+      template: RssDefaultTemplate,
+    },
+    grid: {
+      label: 'Grid',
+      template: RssGridTemplate,
+    },
+    summary: {
+      label: 'List with image',
+      template: RssSummaryTemplate,
+    },
+  };
   return config;
 }

51d20209974764660df74722a6119f1d4d7d5493
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-08-25 16:45:27 +0200

    Move https://github.com/kitconcept/volto-light-theme/pull/614 to here

--- frontend/packages/volto-intranet/src/index.ts
+++ frontend/packages/volto-intranet/src/index.ts
@@ -1,12 +1,19 @@
-import { ConfigType } from '@plone/registry';
+import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
 import installSlots from './config/slots';
 import installWidgets from './config/widgets';
 import installBlocks from './config/blocks';
+import type { CustomInheritBehavior, BlocksConfigSettings } from './types';
 
 declare module '@plone/types' {
   export interface GetSiteResponse {
     'kitconcept.intranet.custom_css': string;
+  }
+
+  export interface Expanders {
+    inherit: {
+      'kitconcept.blocks.config': CustomInheritBehavior<BlocksConfigSettings>;
+    };
   }
 }
 

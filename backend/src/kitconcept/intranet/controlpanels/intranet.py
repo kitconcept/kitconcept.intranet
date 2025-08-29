fa52575701e2e3386ed9fbafaffcc81657a1cc32
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2025-08-29 15:23:55 -0600

    add allowed domains setting

--- backend/src/kitconcept/intranet/controlpanels/intranet.py
+++ backend/src/kitconcept/intranet/controlpanels/intranet.py
@@ -40,10 +40,22 @@ class IIntranetSettings(Interface):
         description=_("Enable squared images for person teasers and listings."),
         required=False,
         default="disabled",
-        vocabulary=SimpleVocabulary([
-            SimpleTerm("disabled", "disabled", _("Disabled")),
-            SimpleTerm("squared4to5", "squared4to5", _("4:5")),
-        ]),
+        vocabulary=SimpleVocabulary(
+            [
+                SimpleTerm("disabled", "disabled", _("Disabled")),
+                SimpleTerm("squared4to5", "squared4to5", _("4:5")),
+            ]
+        ),
+    )
+
+    iframe_allowed_domains = schema.List(
+        title=_("Allowed Iframe Domains"),
+        description="A list of allowed domains for iframes. Example: ['example.com']",
+        min_length=0,
+        value_type=schema.TextLine(
+            title="Domain", description="Allowed domain", default="example.com"
+        ),
+        required=False,
     )
 
 
@@ -89,3 +101,4 @@ class IntranetSiteEndpointExpander:
         )
         data["kitconcept.intranet.custom_css"] = settings.custom_css
         data["kitconcept.person_squared_images"] = settings.person_squared_images
+        data["kitconcept.intranet.iframe_allowed_domains"] = settings.iframe_allowed_domains

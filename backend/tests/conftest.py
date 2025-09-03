48832d73b3260f268e5eecb50f0b6edbffec50bf
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-09-03 13:04:23 +0200

    upgrade to k.core 1a20

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -95,7 +95,7 @@ def current_versions() -> CurrentVersions:
     from kitconcept.core import __version__
 
     return CurrentVersions(
-        base="20250612001",
+        base="20250902001",
         dependencies="1000",
         package=__version__,
     )

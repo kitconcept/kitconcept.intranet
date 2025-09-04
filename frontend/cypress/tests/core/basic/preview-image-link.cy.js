24030dc2868a72f3cf26b3bc3661d82a9aac0504
Tishasoumya-02 <tishasoumya@gmail.com>
2025-09-04 19:36:11 +0530

    cypress test

---
+++ frontend/cypress/tests/core/basic/preview-image-link.cy.js
@@ -0,0 +1,71 @@
+describe('Preview Image Link Tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `/**/*?expand*`).as('content');
+    cy.intercept('GET', '/**/Document').as('schema');
+    cy.intercept('POST', '*').as('saveImage');
+    cy.intercept('GET', '/**/image.png/@@images/image-*').as('getImage');
+    cy.viewport('macbook-16');
+    cy.createContent({
+      contentType: 'Document',
+      contentId: 'document',
+      contentTitle: 'Document',
+    });
+    cy.createContent({
+      contentType: 'Image',
+      contentId: 'image',
+      contentTitle: 'My Image',
+    });
+
+    cy.autologin();
+    cy.visit('/');
+    cy.wait('@content');
+  });
+  it('Add preview image via upload', function () {
+    cy.navigate('/document/edit');
+    cy.wait('@schema');
+    cy.get('#metadataform-fieldset-preview_image').within(() => {
+      cy.get('input[type="file"]').attachFile('image.png', {
+        subjectType: 'input',
+        encoding: 'utf8',
+      });
+    });
+    cy.waitForResourceToLoad('image.png/@@images/image');
+    cy.get('#toolbar-save').click();
+
+    cy.wait('@saveImage');
+    cy.wait('@getImage');
+    cy.wait('@content');
+
+    cy.visit('/document/edit');
+
+    cy.get('#metadataform-fieldset-preview_image').within(() => {
+      cy.get('.image-upload-widget-image img')
+        .should('have.attr', 'src')
+        .and('contains', '/document/image.png/@@images/image-');
+      cy.get('.image-upload-widget-image img').should('be.visible');
+    });
+    cy.get('#toolbar-save').click();
+  });
+
+  it('Add preview image from existing Image Content', function () {
+    cy.navigate('/document/edit');
+    cy.wait('@schema');
+
+    cy.get('#metadataform-fieldset-preview_image');
+    cy.get('.toolbar-inner button.ui.basic.icon.button').first().click();
+    cy.findByLabelText('Search SVG').click();
+    cy.get('.ui.input.search').type('My Image');
+    cy.findByLabelText('Select My Image').dblclick();
+
+    cy.get('#toolbar-save').click();
+
+    cy.navigate('/document/edit');
+    cy.get('#metadataform-fieldset-preview_image').within(() => {
+      cy.get('.image-upload-widget-image img')
+        .should('have.attr', 'src')
+        .and('contains', '/image/@@images/image-');
+      cy.get('.image-upload-widget-image img').should('be.visible');
+    });
+    cy.get('#toolbar-save').click();
+  });
+});

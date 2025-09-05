d821a3969bdfdbf8cf8285c2b9b1ec98c6c0ea12
iRohitSingh <61353484+iRohitSingh@users.noreply.github.com>
2025-09-05 23:06:00 +0530

    Add example content of Rss block (#215)
    * Add example content of Rss block
    
    * Rss -> RSS
    
    * Double check the page loaded successfully in a11y tests.
    
    * Avoid warnings about missing default
    
    * format
    
    ---------
    
    Co-authored-by: David Glick <david@glicksoftware.com>

--- frontend/cypress/tests/a11y/accordionBlock.cy.js
+++ frontend/cypress/tests/a11y/accordionBlock.cy.js
@@ -12,7 +12,7 @@ describe('a11y tests', () => {
   // Accordion Block
   it('Accordion Block (/features/block/block-accordion)', () => {
     cy.navigate('/features/block/block-accordion');
-    cy.wait('@content');
+    cy.wait('@content').its('response.statusCode').should('eq', 200);
     cy.injectAxe();
     cy.configureAxe({
       rules: [
--- frontend/cypress/tests/a11y/separatorBlock.cy.js
+++ frontend/cypress/tests/a11y/separatorBlock.cy.js
@@ -12,7 +12,7 @@ describe('a11y tests', () => {
   // separator-block
   it('Separator-block (/features/block/separator-block)', () => {
     cy.navigate('/features/block/separator-block');
-    cy.wait('@content');
+    cy.wait('@content').its('response.statusCode').should('eq', 200);
     cy.injectAxe();
     cy.configureAxe({
       rules: [

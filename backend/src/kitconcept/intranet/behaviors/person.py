fbcd419ba1203070829b6079a089f70b87b8fbf6
Érico Andrei <ericof@gmail.com>
2025-09-09 18:54:20 +0200

    Implement new behavior for Person content type

---
+++ backend/src/kitconcept/intranet/behaviors/person.py
@@ -0,0 +1,29 @@
+from kitconcept.intranet import _
+from plone.autoform.directives import order_after
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope import schema
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class IPersonBehavior(model.Schema):
+    """Behavior: Additional fields for a Person."""
+
+    academic_title = schema.Choice(
+        title=_("Academic Title"),
+        vocabulary="kitconcept.intranet.vocabularies.academic_titles",
+        required=False,
+    )
+
+    job_title = schema.TextLine(
+        title=_("Job Title"),
+        description=_("Please input the job title of the person."),
+        required=False,
+    )
+
+    departament = schema.TextLine(
+        title=_("Department"),
+        description=_("Please input the department of the person."),
+        required=False,
+    )

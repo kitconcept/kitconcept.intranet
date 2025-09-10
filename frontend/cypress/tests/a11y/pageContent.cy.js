298b6eae7b13cc1d790883735b3a5aea101aa9d4
iRohitSingh <61353484+iRohitSingh@users.noreply.github.com>
2025-09-10 13:47:38 +0530

    Fix A11y headlines Unique headings in example content (#227)
    * Fix A11y headlines Unique headings in example content
    * Remove unique headings disabler for most tests

--- frontend/cypress/tests/a11y/pageContent.cy.js
+++ frontend/cypress/tests/a11y/pageContent.cy.js
@@ -14,16 +14,7 @@ describe('a11y tests', () => {
     cy.navigate('/features/content-types/page');
     cy.wait('@content').its('response.statusCode').should('eq', 200);
     cy.injectAxe();
-    cy.configureAxe({
-      rules: [
-        // there are two copies of slate h3,
-        // which have with the same id
-        {
-          id: 'duplicate-id-active',
-          enabled: false,
-        },
-      ],
-    });
+    cy.configureAxe();
     cy.checkAccessibility();
   });
 });

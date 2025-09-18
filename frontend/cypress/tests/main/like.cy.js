2fd411bca983e2c7adc713cd112173b629c0d7c2
iFlameing <ialokkumarsingh0@gmail.com>
2025-09-18 14:39:00 +0530

    fix cypress test

--- frontend/cypress/tests/main/like.cy.js
+++ frontend/cypress/tests/main/like.cy.js
@@ -11,9 +11,17 @@ describe('Like', () => {
     });
     cy.visit('/my-page');
     cy.wait('@content');
+    cy.navigate('/my-page/edit');
+    cy.wait('@content');
   });
 
   it('Test like functionality', () => {
+    cy.get('#sidebar-metadata .field-wrapper-enable_likes input').click({
+      force: true,
+    });
+    cy.get('#toolbar-save').click();
+    cy.wait('@content');
+    cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
     cy.get(".likes-section button[aria-label='liked']").click();
     cy.get('.likes-section .likes-count span').contains('1');
     cy.get(".likes-section button[aria-label='liked']").click();

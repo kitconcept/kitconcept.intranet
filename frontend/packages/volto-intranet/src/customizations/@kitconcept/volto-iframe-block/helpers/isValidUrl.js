8b4cb769ba68863f75878e5b8daa63de4462735f
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2025-09-18 12:43:42 -0600

    remove shadowing for Edit component and shadow validator instead

---
+++ frontend/packages/volto-intranet/src/customizations/@kitconcept/volto-iframe-block/helpers/isValidUrl.js
@@ -0,0 +1,11 @@
+/**
+ * OVERRIDE: isValidUrl.js
+ * REASON: Validating URL against allowed domains
+ * DATE: 2025-09-18
+ * DEVELOPER: @danalvrz
+ */
+
+export {
+  isValidUrl,
+  toastError,
+} from '../../../../components/Blocks/IFrame/isValidUrl';

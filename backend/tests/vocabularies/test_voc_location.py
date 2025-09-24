9a7a5c02f3133869957d284cca9a2c1efbb5ac05
Timo Stollenwerk <tisto@users.noreply.github.com>
2025-09-24 15:27:24 +0200

    Add three location example content objects (#246)
    * Add three location example content objects
    
    * Fix failing test due to example content amendment
    
    * Add changelog
    
    * Move news item to backend

--- backend/tests/vocabularies/test_voc_location.py
+++ backend/tests/vocabularies/test_voc_location.py
@@ -1,8 +1,8 @@
-from kitconcept.intranet.vocabularies.base import CatalogVocabulary
+import pytest
 from plone import api
 from plone.app.vocabularies import SimpleVocabulary
 
-import pytest
+from kitconcept.intranet.vocabularies.base import CatalogVocabulary
 
 
 class TestVocab:
@@ -19,7 +19,7 @@ class TestVocab:
         assert isinstance(self.vocab, self.vocab_type)
 
     @pytest.mark.parametrize(
-        "token,title", [("97e82b07b5444728b1517de15c79fefb", "Headquarters (Bonn)")]
+        "token,title", [("97e82b07b5444728b1517de15c79fefb", "Standort Bonn")]
     )
     def test_vocab_terms(self, token: str, title: str):
         term = self.vocab.getTermByToken(token)

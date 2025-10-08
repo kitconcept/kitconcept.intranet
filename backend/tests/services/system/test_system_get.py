9e9e2003ae7ba23a687398028690f341aed3f6f8
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-10-08 12:35:08 +0200

    Changelog

--- backend/tests/services/system/test_system_get.py
+++ backend/tests/services/system/test_system_get.py
@@ -47,7 +47,7 @@ class TestSystemGet:
         (
             ("cmf_version", "3.7"),
             ("debug_mode", "No"),
-            ("plone_restapi_version", "9.15.1"),
+            ("plone_restapi_version", "9.15.3"),
             ("plone_version", "6.1.3"),
             ("upgrade", False),
         ),

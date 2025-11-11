17abadede33add27181bf2837e190a7706ccdbd9
David Glick <david@glicksoftware.com>
2025-11-11 12:16:27 -0800

    Don't activate solr on install if the solr host is not configured

--- backend/src/kitconcept/intranet/setuphandlers/__init__.py
+++ backend/src/kitconcept/intranet/setuphandlers/__init__.py
@@ -1,6 +1,11 @@
 from kitconcept.intranet.upgrades import remove_preview_image_behavior
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+import os
+from collective.solr.interfaces import ISolrSchema
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from plone import api
 
 
 @implementer(INonInstallable)
@@ -17,3 +22,12 @@ class HiddenProfiles:
 
 def post_install(setup_tool):
     remove_preview_image_behavior(setup_tool)
+
+
+def post_solr_install(setup_tool):
+    """Post install script for kitconcept.intranet:solr profile."""
+    # Activate solr _if_ a solr host is configured
+    if os.environ.get("COLLECTIVE_SOLR_HOST"):
+        api.site.set_registry_record(
+            "collective.solr.interfaces.ISolrSchema.active", True
+        )

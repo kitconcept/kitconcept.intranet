7b2443da46533366c2195d640be1e4b9c5fb50c3
jnptk <110389276+jnptk@users.noreply.github.com>
2026-04-02 15:52:45 +0200

    off source reminder email for better testability

--- backend/scripts/review_reminder.py
+++ backend/scripts/review_reminder.py
@@ -1,27 +1,10 @@
-from datetime import date
 from plone import api
 from zope.component.hooks import setSite
+from kitconcept.intranet.utils.review_due_notifier import nofity_reviewer
 
 
 portal = app.Plone
 setSite(portal)
 
 with api.env.adopt_roles(["Manager"]):
-    brains = portal.portal_catalog.unrestrictedSearchResults(
-        review_due_date=date.today()
-    )
-    for brain in brains:
-        obj = brain.getObject()
-        reviewer = api.user.get(
-            obj.review_assignee if obj.review_assignee else obj.Creator()
-        )
-
-        breakpoint()
-        mail_subject = "Lorem Ipsum"
-        mail_body = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-
-        api.portal.send_email(
-            recipient=reviewer.getProperty("email"),
-            subject=mail_subject,
-            body=mail_body,
-        )
+    nofity_reviewer(portal)
---
+++ backend/src/kitconcept/intranet/utils/review_due_notifier.py
@@ -0,0 +1,47 @@
+from datetime import date
+from plone import api
+
+import logging
+
+logger = logging.getLogger("kitconcept.intranet")
+
+
+def nofity_reviewer(portal):
+    brains = portal.portal_catalog.unrestrictedSearchResults(
+        review_due_date=date.today()
+    )
+    for brain in brains:
+        obj = brain.getObject()
+        reviewer = api.user.get(
+            obj.review_assignee if obj.review_assignee else obj.Creator()
+        )
+
+        if not reviewer:
+            logger.warning("Couldn't find user. No mail will be sent.")
+            return
+
+        breakpoint()
+        mail_subject = f"🔔 Reminder: Content review due for {obj.Title()}"
+        mail_body = (
+            f"Hello {reviewer.getProperty('fullname') or reviewer.getUserName()},"
+            f"The content item “{obj.Title()}” is due for review."
+            "Please check whether the information is still accurate and up to date."
+            f"Last updated: {obj}"
+            "Next review date (after completion): will be recalculated automatically"
+            "You can open the content here:"
+            f"👉 {obj.absolute_url()}"
+            "Available actions:"
+            "- ✅ Review the content and mark as reviewed"
+            "- 🕓 Postpone next review (e.g., by 3 or 6 months)"
+            "- 📝 Mark as “changes required” if updates are needed"
+            "Thank you for keeping our content accurate and relevant."
+            "Kind regards,"
+            "Your intranet team"
+        )
+
+        api.portal.send_email(
+            recipient=reviewer.getProperty("email"),
+            subject=mail_subject,
+            body=mail_body,
+            immediate=True,
+        )

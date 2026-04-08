2d9a4baabbd665e962112da8a02981de5dc22639
David Glick <david@glicksoftware.com>
2026-04-08 10:14:25 -0700

    Remove unneeded explicit commits, disable CSRF protection

--- backend/src/kitconcept/intranet/services/review.py
+++ backend/src/kitconcept/intranet/services/review.py
@@ -2,13 +2,14 @@ from datetime import date
 from kitconcept.intranet.behaviors.content_review import IContentReview
 from kitconcept.intranet.utils.calc_due_date import calc_due_date
 from plone import api
+from plone.protect.interfaces import IDisableCSRFProtection
 from plone.restapi.deserializer import json_body
 from plone.restapi.services import Service
 from zExceptions import BadRequest
+from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 
-import transaction
 
 
 @implementer(IPublishTraverse)
@@ -25,6 +26,9 @@ class ReviewPost(Service):
         return self
 
     def reply(self):
+        # Disable CSRF protection
+        alsoProvides(self.request, IDisableCSRFProtection)
+
         match self.params:
             case ["approve"]:
                 # update review_status
@@ -37,7 +41,6 @@ class ReviewPost(Service):
                 self.context.review_due_date = calc_due_date(interval=interval)
                 # update review_completed_date
                 self.context.review_completed_date = date.today()
-                transaction.commit()
             case ["delegate"]:
                 field = IContentReview["review_assignee"].bind(self.context)
                 vocabulary = field.vocabulary
@@ -48,7 +51,6 @@ class ReviewPost(Service):
                 if assignee not in vocabulary:
                     raise BadRequest(f"Assignee not found in vocabulary: {vocabulary}")
                 self.context.review_assignee = assignee
-                transaction.commit()
             case ["postpone"]:
                 self.context.review_status = "Up-to-date"
                 data = json_body(self.request)
@@ -57,7 +59,6 @@ class ReviewPost(Service):
                 due_date = data.get("due_date", None)
                 if due_date:
                     self.context.review_due_date = date.fromisoformat(due_date)
-                transaction.commit()
             case _:
                 raise BadRequest(
                     "Unknown action: expected /@review/approve, "

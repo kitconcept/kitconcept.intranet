8c1cd214c87740cbffadcdfcfd1914c72c63af21
Tishasoumya-02 <tishasoumya@gmail.com>
2026-04-28 18:09:25 +0530

    fix lint

--- frontend/packages/kitconcept-intranet/src/components/Sidebar/PostponeReview.jsx
+++ frontend/packages/kitconcept-intranet/src/components/Sidebar/PostponeReview.jsx
@@ -105,67 +105,67 @@ const PostponeReview = (props) => {
         className="postpone-form"
         defaultExpandedKeys={['postpone-review']}
       >
-          <AccordionItem id="postpone-review">
-            <div className="postpone-form-title">
-              {intl.formatMessage(messages.postponeTitle)}
-            </div>
-            <AccordionPanel className="postpone-form-content">
-              <p>{intl.formatMessage(messages.postponeReview)}</p>
-              <CheckboxWidget
-                id="usePresetReviewInterval"
-                title={intl.formatMessage(messages.usePresetReviewInterval)}
-                value={presetReviewInterval}
-                onChange={(name, value) => {
-                  setPresetReviewInterval(value);
+        <AccordionItem id="postpone-review">
+          <div className="postpone-form-title">
+            {intl.formatMessage(messages.postponeTitle)}
+          </div>
+          <AccordionPanel className="postpone-form-content">
+            <p>{intl.formatMessage(messages.postponeReview)}</p>
+            <CheckboxWidget
+              id="usePresetReviewInterval"
+              title={intl.formatMessage(messages.usePresetReviewInterval)}
+              value={presetReviewInterval}
+              onChange={(name, value) => {
+                setPresetReviewInterval(value);
+              }}
+            />
+
+            <p className="help">
+              {intl.formatMessage(messages.help, {
+                interval: review_interval?.title ?? '0',
+              })}
+            </p>
+            <DatetimeWidget
+              id="nextReviewDate"
+              title={intl.formatMessage(messages.nextReview)}
+              fieldSet="default"
+              dateOnly={true}
+              noPastDates={true}
+              isDisabled={presetReviewInterval}
+              onChange={(id, value) => setDueDate(value)}
+              value={!presetReviewInterval ? dueDate : review_due_date}
+            />
+
+            <TextareaWidget
+              id="review-comment"
+              title={intl.formatMessage(messages.comment)}
+              fieldSet="default"
+              rows={3}
+              style={{ height: 'initial' }}
+              onChange={(id, value) => setComment(value)}
+              value={comment}
+              isDisabled={false}
+              placeholder={intl.formatMessage(messages.commentPlaceholder)}
+            />
+
+            <div className="postpone-review-buttons">
+              <Button
+                type="button"
+                className="cancel-button"
+                title={intl.formatMessage(messages.cancel)}
+                basic
+                onClick={() => {
+                  onClose();
                 }}
-              />
-
-              <p className="help">
-                {intl.formatMessage(messages.help, {
-                  interval: review_interval?.title ?? '0',
-                })}
-              </p>
-              <DatetimeWidget
-                id="nextReviewDate"
-                title={intl.formatMessage(messages.nextReview)}
-                fieldSet="default"
-                dateOnly={true}
-                noPastDates={true}
-                isDisabled={presetReviewInterval}
-                onChange={(id, value) => setDueDate(value)}
-                value={!presetReviewInterval ? dueDate : review_due_date}
-              />
-
-              <TextareaWidget
-                id="review-comment"
-                title={intl.formatMessage(messages.comment)}
-                fieldSet="default"
-                rows={3}
-                style={{ height: 'initial' }}
-                onChange={(id, value) => setComment(value)}
-                value={comment}
-                isDisabled={false}
-                placeholder={intl.formatMessage(messages.commentPlaceholder)}
-              />
-
-              <div className="postpone-review-buttons">
-                <Button
-                  type="button"
-                  className="cancel-button"
-                  title={intl.formatMessage(messages.cancel)}
-                  basic
-                  onClick={() => {
-                    onClose();
-                  }}
-                >
-                  {intl.formatMessage(messages.cancel)}
-                </Button>
-                <Button type="submit" basic className="postpone-button">
-                  {intl.formatMessage(messages.postponeButton)}
-                </Button>
-              </div>
-            </AccordionPanel>
-          </AccordionItem>
+              >
+                {intl.formatMessage(messages.cancel)}
+              </Button>
+              <Button type="submit" basic className="postpone-button">
+                {intl.formatMessage(messages.postponeButton)}
+              </Button>
+            </div>
+          </AccordionPanel>
+        </AccordionItem>
       </Accordion>
     </UiForm>
   );

f2f866e591c4e69bc75a02915939974484ebf164
Érico Andrei <ericof@gmail.com>
2026-07-30 14:57:14 -0300

    Restore the querystring widget depth field (#459)
    Override Volto's QuerystringWidget to reintroduce the top-level Depth
    field, shown when the query contains a path criterion. This reverts
    plone/volto#8350 for the intranet.
    
    Refs https://gitlab.kitconcept.io/kitconcept/distribution-kitconcept-intranet/-/issues/541

---
+++ frontend/packages/kitconcept-intranet/src/customizations/volto/components/manage/Widgets/QuerystringWidget.jsx
@@ -0,0 +1,12 @@
+/**
+ * OVERRIDE: QuerystringWidget.jsx
+ * REASON: Revert PR https://github.com/plone/volto/pull/8350 of volto
+ * FILE VERSION: Volto 19.3.0
+ * DATE: 2026-07-30
+ * DEVELOPER: @ericof
+ * CHANGELOG:
+ *  - 2026-07-30 @ericof: Revert changes made by upstream PR
+ */
+import QuerystringWidget from '@kitconcept/intranet/components/widgets/QuerystringWidget';
+
+export default QuerystringWidget;

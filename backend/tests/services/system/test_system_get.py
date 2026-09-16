ed4c7b20d398306b33c5e9226ea7104a105761e8
Jakob Kahl <44289551+jackahl@users.noreply.github.com>
2026-09-16 13:36:25 +0200

    update plone.restapi to 9.15.7 (#506)
    * update plone.restapi to 9.15.7
    
    * update plone restapi dependencies
    
    * Update pnpm-lock
    
    * Fix test
    
    * Try to fix tests
    
    ---------
    
    Co-authored-by: Victor Fernandez de Alba <sneridagh@gmail.com>

--- backend/tests/services/system/test_system_get.py
+++ backend/tests/services/system/test_system_get.py
@@ -47,7 +47,7 @@ class TestSystemGet:
         (
             ("cmf_version", "3.7"),
             ("debug_mode", "No"),
-            ("plone_restapi_version", "9.15.3"),
+            ("plone_restapi_version", "9.15.7"),
             ("plone_version", "6.1.3"),
             ("upgrade", False),
         ),

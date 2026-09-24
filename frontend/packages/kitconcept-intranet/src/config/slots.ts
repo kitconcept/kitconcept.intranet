20f5592a5ed4100b1e8ea42008236bb19afa6eee
iRohitSingh <isinghrohit0@gmail.com>
2026-09-24 18:54:39 +0530

    improve code

--- frontend/packages/kitconcept-intranet/src/config/slots.ts
+++ frontend/packages/kitconcept-intranet/src/config/slots.ts
@@ -21,8 +21,13 @@ const isWorkspaceDescendant = ({ content }: { content: Content }) =>
 
 function isWorkspaceOrDescendant(contentTypes: string[]): SlotPredicate {
   const contentTypeCondition = ContentTypeCondition(contentTypes);
-  return (args: GetSlotArgs) =>
-    contentTypeCondition(args) || isWorkspaceDescendant(args);
+  return (args: GetSlotArgs) => {
+    const isAddView = args.location.pathname.endsWith('/add');
+
+    return (
+      !isAddView && (contentTypeCondition(args) || isWorkspaceDescendant(args))
+    );
+  };
 }
 
 function shouldShowContentInteractions(args: GetSlotArgs): boolean {
@@ -98,14 +103,7 @@ export default function install(config: ConfigType) {
     slot: 'aboveApp',
     name: 'NavigationTree2',
     component: NavigationTreePortal,
-    predicates: [
-      isWorkspaceOrDescendant(['WikiPage', 'Workspace']),
-      ({ location }) =>
-        !(
-          location.pathname.endsWith('/add') &&
-          new URLSearchParams(location.search).get('type') === 'Workspace'
-        ),
-    ],
+    predicates: [isWorkspaceOrDescendant(['WikiPage', 'Workspace'])],
   });
   config.registerSlotComponent({
     slot: 'aboveApp',
